519ee28130d2d58fcf84793ed21a8cafca7028c0
Daniel Meseguer <daniel@meseguer.biz>
2020-11-07 01:39:28 -0600

    Login and signup

--- Veterinary/Appointments/views.py
+++ Veterinary/Appointments/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Pet
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 
 # Create your views here.
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
 
+        return redirect("/home")
+    else:
+        form = RegisterForm()
+
+    return render(response, "register/register.html", {"form":form})
+
+# Create your views here.
+def init(request):
+    return render(request, "init.html")
+    
 def home(request):
-    return HttpResponse('Showing "home" page')
+    return render(request, "home.html")
 
 def new(request):
     return HttpResponse('Showing "new" page')
--- Veterinary/Veterinary/settings.py
+++ Veterinary/Veterinary/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Appointments.apps.AppointmentsConfig',
     'widget_tweaks',
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
+LOGIN_REDIRECT_URL = "/home/"
+LOGOUT_REDIRECT_URL = "/"
